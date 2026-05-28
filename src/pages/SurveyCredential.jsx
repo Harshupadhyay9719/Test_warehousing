6ca146210e2413628b02456a8b629a7d314eb0e3
Harshupadhyay9719 <upadhyayhb2006@gmail.com>
2026-05-28 16:57:39 +0530

    changed name of pages

--- src/pages/SurveyCredential.jsx
+++ src/pages/SurveyCredential.jsx
@@ -1,6 +1,6 @@
 import SurveyApp from '../components/SurveyApp.jsx';
 
-export default function Survey({ initialScreen = 'survey', respondent, onFinish }) {
+export default function SurveyCredential({ initialScreen = 'survey', respondent, onFinish }) {
   return (
     <section id="survey">
       <SurveyApp initialScreen={initialScreen} respondent={respondent} onFinish={onFinish} />
